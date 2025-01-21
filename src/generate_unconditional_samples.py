06362d1e7cd1b61d6bb99ecc23a2e952cafceb87
Mandar Chandorkar <mandar2812@gmail.com>
2025-01-21 16:34:38 +0100

    Migrated to TF2 python 3.12

--- src/generate_unconditional_samples.py
+++ src/generate_unconditional_samples.py
@@ -6,10 +6,11 @@ import os
 import numpy as np
 import tensorflow as tf
 
-import model, sample, encoder
+from src import model, sample, encoder
+
 
 def sample_model(
-    model_name='124M',
+    model_name="124M",
     seed=None,
     nsamples=0,
     batch_size=1,
@@ -17,7 +18,7 @@ def sample_model(
     temperature=1,
     top_k=0,
     top_p=1,
-    models_dir='models',
+    models_dir="models",
 ):
     """
     Run the sample_model
@@ -43,23 +44,28 @@ def sample_model(
     models_dir = os.path.expanduser(os.path.expandvars(models_dir))
     enc = encoder.get_encoder(model_name, models_dir)
     hparams = model.default_hparams()
-    with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+    with open(os.path.join(models_dir, model_name, "hparams.json")) as f:
         hparams.override_from_dict(json.load(f))
 
     if length is None:
         length = hparams.n_ctx
     elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+        raise ValueError(
+            "Can't get samples longer than window size: %s" % hparams.n_ctx
+        )
 
     with tf.Session(graph=tf.Graph()) as sess:
         np.random.seed(seed)
         tf.set_random_seed(seed)
 
         output = sample.sample_sequence(
-            hparams=hparams, length=length,
-            start_token=enc.encoder['<|endoftext|>'],
+            hparams=hparams,
+            length=length,
+            start_token=enc.encoder["<|endoftext|>"],
             batch_size=batch_size,
-            temperature=temperature, top_k=top_k, top_p=top_p
+            temperature=temperature,
+            top_k=top_k,
+            top_p=top_p,
         )[:, 1:]
 
         saver = tf.train.Saver()
@@ -75,6 +81,6 @@ def sample_model(
                 print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
                 print(text)
 
-if __name__ == '__main__':
-    fire.Fire(sample_model)
 
+if __name__ == "__main__":
+    fire.Fire(sample_model)
--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -6,10 +6,11 @@ import os
 import numpy as np
 import tensorflow as tf
 
-import model, sample, encoder
+from src import model, sample, encoder
+
 
 def interact_model(
-    model_name='124M',
+    model_name="124M",
     seed=None,
     nsamples=1,
     batch_size=1,
@@ -17,7 +18,7 @@ def interact_model(
     temperature=1,
     top_k=0,
     top_p=1,
-    models_dir='models',
+    models_dir="models",
 ):
     """
     Interactively run the model
@@ -46,40 +47,46 @@ def interact_model(
 
     enc = encoder.get_encoder(model_name, models_dir)
     hparams = model.default_hparams()
-    with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+    with open(os.path.join(models_dir, model_name, "hparams.json")) as f:
         hparams.override_from_dict(json.load(f))
 
     if length is None:
         length = hparams.n_ctx // 2
     elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
-
-    with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
-        np.random.seed(seed)
-        tf.set_random_seed(seed)
-        output = sample.sample_sequence(
-            hparams=hparams, length=length,
-            context=context,
-            batch_size=batch_size,
-            temperature=temperature, top_k=top_k, top_p=top_p
+        raise ValueError(
+            "Can't get samples longer than window size: %s" % hparams.n_ctx
         )
 
-        saver = tf.train.Saver()
+    with tf.compat.v1.Session(graph=tf.Graph()) as sess:
+        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
+        np.random.seed(seed)
+        tf.random.set_seed(seed)
+        output = sample.sample_sequence(
+            hparams=hparams,
+            length=length,
+            context=context,
+            batch_size=batch_size,
+            temperature=temperature,
+            top_k=top_k,
+            top_p=top_p,
+        )
+
+        saver = tf.compat.v1.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
 
         while True:
             raw_text = input("Model prompt >>> ")
             while not raw_text:
-                print('Prompt should not be empty!')
+                print("Prompt should not be empty!")
                 raw_text = input("Model prompt >>> ")
             context_tokens = enc.encode(raw_text)
             generated = 0
             for _ in range(nsamples // batch_size):
-                out = sess.run(output, feed_dict={
-                    context: [context_tokens for _ in range(batch_size)]
-                })[:, len(context_tokens):]
+                out = sess.run(
+                    output,
+                    feed_dict={context: [context_tokens for _ in range(batch_size)]},
+                )[:, len(context_tokens) :]
                 for i in range(batch_size):
                     generated += 1
                     text = enc.decode(out[i])
@@ -87,6 +94,6 @@ def interact_model(
                     print(text)
             print("=" * 80)
 
-if __name__ == '__main__':
-    fire.Fire(interact_model)
 
+if __name__ == "__main__":
+    fire.Fire(interact_model)
